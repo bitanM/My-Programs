023b42b38986e8e4a7f7eec9f4a08c565d40d071
BITAN MAJUMDER <108423919+bitanM@users.noreply.github.com>
2022-11-01 15:09:09 +0530

    Create selection_sort.c

---
+++ selection_sort.c
@@ -0,0 +1,24 @@
+
+int main(){
+	int el;
+	printf("Enter the number of elements: ");
+	scanf("%d", &el);
+	
+	int* arr = (int*)malloc(el*sizeof(int));
+	for(int i=0; i<el; i++){
+		scanf("%d", &arr[i]);
+	}
+	for(int i=0; i<el; i++){
+		int min = i;
+		for(int j=i; j<el; j++){
+			if(arr[j]<arr[min])
+				min = j;
+		}
+		int temp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
+	}
+	for(int i=0; i<el; i++){
+		printf("%d ", arr[i]);
+	}
+}
