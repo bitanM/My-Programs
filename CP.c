b0d9737cf83748884ff8b9bd40c743cad04e13ad
BITAN MAJUMDER <108423919+bitanM@users.noreply.github.com>
2022-11-18 16:16:19 +0000

    Nw

---
+++ CP.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char Y[] = "";
+    int n = 20;
+    for(int i=0; i<n; i++){
+        Y[i]="Yes";
+    }
+}
--- Python Files/test.py
+++ Python Files/test.py
@@ -1,4 +1,18 @@
-f=open('text.txt')
-for x in f:
-    print(x)
-
+m = int(input())
+n = int(input())
+lst = []
+for i in range(n):
+    mul = m*n
+    if(mul%10==0):
+        lst.append(mul)
+    while len(lst) > 1:
+        for j in range(len(lst)):
+            for i in lst:
+                if(lst[i]%10==0):
+                        lst[i] = lst/10
+                else:
+                        lst.remove(lst[i])
+    if(len(lst)>1):
+        ans = max(lst)
+    elif(len(lst)==0):
+        ans = 0
