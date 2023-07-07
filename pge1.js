53b0e7d35f9c84f1c4310e440bc638f6e669dd36
BITAN MAJUMDER <108423919+bitanM@users.noreply.github.com>
2023-07-07 12:19:43 +0530

    Add files via upload

---
+++ pge1.js
@@ -0,0 +1,20 @@
+
+document.write("Welcome to Java Script")
+	function message(){
+			alert("Click to see in English")
+			printA()
+			add(a, b)
+		}
+var v1, v2
+v1 = 4
+v2 = 6
+document.write(v1+v2)
+function printA(){
+	var a = parseInt(prompt("Enter the value of A: "));
+	var b = parseInt(prompt("Enter the value of B: "));
+	document.write(a+b);
+}
+
+
+
+ 
