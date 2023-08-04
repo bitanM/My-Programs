c8bd4abaa8ce8493d25c21162e0c587355de5f99
BITAN MAJUMDER <108423919+bitanM@users.noreply.github.com>
2023-08-04 21:35:18 +0530

    Update Client.java

--- Internet Technologies/HD/bean.com/Client.java
+++ Internet Technologies/HD/bean.com/Client.java
@@ -1,4 +1,4 @@
-package Bean.com;
+import IT.*;
 
 public class Client {
 	public static void main(String[] args) {
@@ -8,6 +8,8 @@ public class Client {
 		bs.set_Account_no(18824850);
 		bs.set_password("snowy@marlinspike");
 		bs.set_balance(80000);
+		bs.credit(100);
+		bs.debit(400);
 		Client c = new Client();
 		c.printClient(bs);
 	}
